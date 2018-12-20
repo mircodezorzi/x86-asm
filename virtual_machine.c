fc32997a3424865068e915894e940ba0a2fdd0de
Mirco De Zorzi <mircodezorzi@protonmail.com>
2018-12-20 22:59:18 +0100

    reworked registry related functions and added flags

--- virtual_machine.c
+++ virtual_machine.c
@@ -5,28 +5,37 @@
 
 #include "virtual_machine.h"
 
+#ifdef MEMORY_RANDOMIZE_AT_INIT
+#include<time.h>
+#endif
+
 virtual_machine *
-virtual_machine_new(uint32_t memory_size)
-{
+virtual_machine_new(uint32_t memory_size){
     virtual_machine* vm = (virtual_machine*)malloc(sizeof(virtual_machine*));
     vm->memory = (int8_t*)malloc(sizeof(int8_t*) * memory_size * 8);
     vm->memory_size = memory_size;
-#ifdef MEMORY_CLEAR_AT_INIT
+    #ifdef MEMORY_CLEAR_AT_INIT
     memset(vm->memory, 0, memory_size * 8);
-#endif
+    #elif defined(MEMORY_RANDOMIZE_AT_INIT)
+    srand(time(NULL));
+    for(uint32_t i = 0; i < memory_size * 8; i++)
+        vm->memory[i] = rand() % 255;
+    #endif
+    memset(vm->registers, 0, 11);
+    vm->MAR = 0;
+    vm->PC  = 0;
+    vm->IP  = 0;
     return vm;
 }
 
 void
-virtual_machine_destroy(virtual_machine* vm)
-{
+virtual_machine_destroy(virtual_machine* vm){
     free(vm->memory);
     free(vm);
 }
 
 void
-vm_memory_print(virtual_machine* vm, uint32_t lower, uint32_t upper)
-{
+vm_memory_print(virtual_machine* vm, uint32_t lower, uint32_t upper){
     for(uint16_t i = lower; upper > 0
             ? i < upper * 8
             : i < 40; i += 8){
@@ -47,107 +56,89 @@ vm_memory_print(virtual_machine* vm, uint32_t lower, uint32_t upper)
 }
 
 int32_t
-vm_register_get(virtual_machine* vm, registry reg)
-{
+vm_register_get(virtual_machine* vm, registry reg){
     switch(reg){
-        // general purpose registers
         case EAX: case AX: case AL: return vm->registers[0];
         case EBX: case BX: case BL: return vm->registers[1];
         case ECX: case CX: case CL: return vm->registers[2];
         case EDX: case DX: case DL: return vm->registers[3];
-        case AH: return vm->registers[0] >> 8;
-        case BH: return vm->registers[1] >> 8;
-        case CH: return vm->registers[2] >> 8;
-        case DH: return vm->registers[3] >> 8;
-
-        // stack registers
-        case EDS: case DS: return vm->registers[4];
-        case EES: case ES: return vm->registers[5];
-        case EFS: case FS: return vm->registers[6];
-        case EGS: case GS: return vm->registers[7];
-        case ESS: case SS: return vm->registers[8];
-        case ECS: case CS: return vm->registers[9];
-        case EIP: case IP: return vm->registers[10];
-
-        // instructions
-        case MAR: return vm->registers[11];
-        case PC:  return vm->registers[12];
+        case AH:                    return vm->registers[0] >> 8;
+        case BH:                    return vm->registers[1] >> 8;
+        case CH:                    return vm->registers[2] >> 8;
+        case DH:                    return vm->registers[3] >> 8;
+        case ESI: case SI:          return vm->registers[4];
+        case EDI: case DI:          return vm->registers[5];
+        case EBP: case BP:          return vm->registers[6];
+        case ESP: case SP:          return vm->registers[7];
+        case CS:                    return vm->registers[8] >> 8;
+        case DS:                    return vm->registers[9] >> 8;
+        case ES:                    return vm->registers[10]>> 8;
+        case FS:                    return vm->registers[8];
+        case GS:                    return vm->registers[9];
+        case SS:                    return vm->registers[10];
     }
 }
 
-#define CB(reg, arg) case reg: arg; break
+#define CB(cas, arg) case cas: arg; break
 
 void
-vm_register_set(virtual_machine* vm, registry reg, int32_t value)
-{
+vm_register_set(virtual_machine* vm, registry reg, int32_t value){
     switch(reg){
-        // general purpose registers
         CB(EAX, vm->registers[0] = value);
         CB(EBX, vm->registers[1] = value);
         CB(ECX, vm->registers[2] = value);
         CB(EDX, vm->registers[3] = value);
-        CB(AX,  vm->registers[0] = (vm->registers[0] & 0xFF00) + value);
-        CB(BX,  vm->registers[1] = (vm->registers[1] & 0xFF00) + value);
-        CB(CX,  vm->registers[2] = (vm->registers[2] & 0xFF00) + value);
-        CB(DX,  vm->registers[3] = (vm->registers[3] & 0xFF00) + value);
-        CB(AL,  vm->registers[0] = (vm->registers[0] & 0xFFF0) + value);
-        CB(BL,  vm->registers[1] = (vm->registers[1] & 0xFFF0) + value);
-        CB(CL,  vm->registers[2] = (vm->registers[2] & 0xFFF0) + value);
-        CB(DL,  vm->registers[3] = (vm->registers[3] & 0xFFF0) + value);
-        CB(AH,  vm->registers[0] = (vm->registers[0] & 0xFF0F) + value << 8);
-        CB(BH,  vm->registers[1] = (vm->registers[1] & 0xFF0F) + value << 8);
-        CB(CH,  vm->registers[2] = (vm->registers[2] & 0xFF0F) + value << 8);
-        CB(DH,  vm->registers[3] = (vm->registers[3] & 0xFF0F) + value << 8);
-
-        // stack registers
-        CB(EDS, vm->registers[4] = value);
-        CB(EES, vm->registers[6] = value);
-        CB(EFS, vm->registers[5] = value);
-        CB(EGS, vm->registers[7] = value);
-        CB(ESS, vm->registers[8] = value);
-        CB(ECS, vm->registers[9] = value);
-        CB(EIP, vm->registers[10] = value);
-        CB(DS,  vm->registers[4] = (vm->registers[4] & 0xFF00) + value);
-        CB(ES,  vm->registers[5] = (vm->registers[5] & 0xFF00) + value);
-        CB(FS,  vm->registers[6] = (vm->registers[6] & 0xFF00) + value);
-        CB(GS,  vm->registers[7] = (vm->registers[7] & 0xFF00) + value);
-        CB(SS,  vm->registers[8] = (vm->registers[8] & 0xFF00) + value);
-        CB(CS,  vm->registers[9] = (vm->registers[8] & 0xFF00) + value);
-        CB(IP,  vm->registers[10] = (vm->registers[8] & 0xFF00) + value);
-
-        // instructions
-        CB(MAR, vm->registers[11] = value);
-        CB(PC,  vm->registers[12] = value);
+        CB(AX,  vm->registers[0] = (vm->registers[0] & 0xFFFF0000) + value);
+        CB(BX,  vm->registers[1] = (vm->registers[1] & 0xFFFF0000) + value);
+        CB(CX,  vm->registers[2] = (vm->registers[2] & 0xFFFF0000) + value);
+        CB(DX,  vm->registers[3] = (vm->registers[3] & 0xFFFF0000) + value);
+        CB(AL,  vm->registers[0] = (vm->registers[0] & 0xFFFFFF00) + value);
+        CB(BL,  vm->registers[1] = (vm->registers[1] & 0xFFFFFF00) + value);
+        CB(CL,  vm->registers[2] = (vm->registers[2] & 0xFFFFFF00) + value);
+        CB(DL,  vm->registers[3] = (vm->registers[3] & 0xFFFFFF00) + value);
+        CB(AH,  vm->registers[0] = (vm->registers[0] & 0xFFFF00FF) + (value << 8));
+        CB(BH,  vm->registers[1] = (vm->registers[1] & 0xFFFF00FF) + (value << 8));
+        CB(CH,  vm->registers[2] = (vm->registers[2] & 0xFFFF00FF) + (value << 8));
+        CB(DH,  vm->registers[3] = (vm->registers[3] & 0xFFFF00FF) + (value << 8));
+        CB(ESI, vm->registers[4] = value);
+        CB(EDI, vm->registers[5] = value);
+        CB(EBP, vm->registers[6] = value);
+        CB(ESP, vm->registers[7] = value);
+        CB(SI,  vm->registers[4] = (vm->registers[4] & 0xFFFF0000) + value);
+        CB(DI,  vm->registers[5] = (vm->registers[5] & 0xFFFF0000) + value);
+        CB(BP,  vm->registers[6] = (vm->registers[6] & 0xFFFF0000) + value);
+        CB(SP,  vm->registers[7] = (vm->registers[7] & 0xFFFF0000) + value);
+        CB(FS,  vm->registers[8] = (vm->registers[8] & 0xFFFF0000) + value);
+        CB(GS,  vm->registers[9] = (vm->registers[9] & 0xFFFF0000) + value);
+        CB(SS,  vm->registers[10]= (vm->registers[10]& 0xFFFF0000) + value);
+        CB(CS,  vm->registers[8] = (vm->registers[8] & 0x0000FFFF) + (value << 8));
+        CB(DS,  vm->registers[9] = (vm->registers[9] & 0x0000FFFF) + (value << 8));
+        CB(ES,  vm->registers[10]= (vm->registers[10]& 0x0000FFFF) + (value << 8));
     }
 }
 
 int8_t
-vm_memory_get_byte(virtual_machine* vm, uint32_t address)
-{
+vm_memory_get_byte(virtual_machine* vm, uint32_t address){
     return  vm->memory[address];
 }
 
 int16_t
-vm_memory_get_word(virtual_machine* vm, uint32_t address)
-{
-    return  vm->memory[address    ] << 8
-          + vm->memory[address + 1];
+vm_memory_get_word(virtual_machine* vm, uint32_t address){
+    return  (vm->memory[address    ] << 8)
+          +  vm->memory[address + 1];
 }
 
 int32_t
-vm_memory_get_dword(virtual_machine* vm, uint32_t address)
-{
-    return  vm->memory[address    ] << 24
-          + vm->memory[address + 1] << 16
-          + vm->memory[address + 2] << 8
-          + vm->memory[address + 3];
+vm_memory_get_dword(virtual_machine* vm, uint32_t address){
+    return  (vm->memory[address    ] << 24)
+          + (vm->memory[address + 1] << 16)
+          + (vm->memory[address + 2] << 8)
+          +  vm->memory[address + 3];
 }
 
 void
-vm_memory_set_register(virtual_machine* vm, registry reg, uint32_t address)
-{
+vm_memory_set_register(virtual_machine* vm, registry reg, uint32_t address){
     switch(reg){
-        // general purpose registers
         case EAX:
             vm->memory[address    ] = vm->registers[0] >> 24;
             vm->memory[address + 1] = vm->registers[0] >> 16;
@@ -159,10 +150,10 @@ vm_memory_set_register(virtual_machine* vm, registry reg, uint32_t address)
             vm->memory[address + 1] = vm->registers[0];
             break;
         case AH:
-            vm->memory[address] = vm->registers[0] >> 8;
+            vm->memory[address    ] = vm->registers[0] >> 8;
             break;
         case AL:
-            vm->memory[address] = vm->registers[0];
+            vm->memory[address    ] = vm->registers[0];
             break;
         case EBX:
             vm->memory[address    ] = vm->registers[1] >> 24;
@@ -175,10 +166,10 @@ vm_memory_set_register(virtual_machine* vm, registry reg, uint32_t address)
             vm->memory[address + 1] = vm->registers[1];
             break;
         case BH:
-            vm->memory[address] = vm->registers[1] >> 8;
+            vm->memory[address    ] = vm->registers[1] >> 8;
             break;
         case BL:
-            vm->memory[address] = vm->registers[1];
+            vm->memory[address    ] = vm->registers[1];
             break;
         case ECX:
             vm->memory[address    ] = vm->registers[2] >> 24;
@@ -191,10 +182,10 @@ vm_memory_set_register(virtual_machine* vm, registry reg, uint32_t address)
             vm->memory[address + 1] = vm->registers[2];
             break;
         case CH:
-            vm->memory[address] = vm->registers[2] >> 8;
+            vm->memory[address    ] = vm->registers[2] >> 8;
             break;
         case CL:
-            vm->memory[address] = vm->registers[2];
+            vm->memory[address    ] = vm->registers[2];
             break;
         case EDX:
             vm->memory[address    ] = vm->registers[3] >> 24;
@@ -207,82 +198,96 @@ vm_memory_set_register(virtual_machine* vm, registry reg, uint32_t address)
             vm->memory[address + 1] = vm->registers[3];
             break;
         case DH:
-            vm->memory[address] = vm->registers[3] >> 8;
+            vm->memory[address    ] = vm->registers[3] >> 8;
             break;
         case DL:
-            vm->memory[address] = vm->registers[3];
+            vm->memory[address    ] = vm->registers[3];
             break;
-
-        // stack registers
-        case EDS:
+        case ESI:
             vm->memory[address    ] = vm->registers[4] >> 24;
             vm->memory[address + 1] = vm->registers[4] >> 16;
             vm->memory[address + 2] = vm->registers[4] >> 8;
             vm->memory[address + 3] = vm->registers[4];
             break;
-        case DS:
+        case SI:
             vm->memory[address    ] = vm->registers[4] >> 8;
             vm->memory[address + 1] = vm->registers[4];
             break;
-        case EES:
+        case EDI:
             vm->memory[address    ] = vm->registers[5] >> 24;
             vm->memory[address + 1] = vm->registers[5] >> 16;
             vm->memory[address + 2] = vm->registers[5] >> 8;
             vm->memory[address + 3] = vm->registers[5];
             break;
-        case ES:
+        case DI:
             vm->memory[address    ] = vm->registers[5] >> 8;
             vm->memory[address + 1] = vm->registers[5];
             break;
-        case EFS:
+        case EBP:
             vm->memory[address    ] = vm->registers[6] >> 24;
             vm->memory[address + 1] = vm->registers[6] >> 16;
             vm->memory[address + 2] = vm->registers[6] >> 8;
             vm->memory[address + 3] = vm->registers[6];
             break;
-        case FS:
+        case BP:
             vm->memory[address    ] = vm->registers[6] >> 8;
             vm->memory[address + 1] = vm->registers[6];
             break;
-        case EGS:
+        case ESP:
             vm->memory[address    ] = vm->registers[7] >> 24;
             vm->memory[address + 1] = vm->registers[7] >> 16;
             vm->memory[address + 2] = vm->registers[7] >> 8;
             vm->memory[address + 3] = vm->registers[7];
             break;
-        case GS:
+        case SP:
             vm->memory[address    ] = vm->registers[7] >> 8;
             vm->memory[address + 1] = vm->registers[7];
             break;
-        case ESS:
-            vm->memory[address    ] = vm->registers[8] >> 24;
-            vm->memory[address + 1] = vm->registers[8] >> 16;
-            vm->memory[address + 2] = vm->registers[8] >> 8;
-            vm->memory[address + 3] = vm->registers[8];
-            break;
-        case SS:
+        case FS:
             vm->memory[address    ] = vm->registers[8] >> 8;
             vm->memory[address + 1] = vm->registers[8];
             break;
-        case ECS:
-            vm->memory[address    ] = vm->registers[9] >> 24;
-            vm->memory[address + 1] = vm->registers[9] >> 16;
-            vm->memory[address + 2] = vm->registers[9] >> 8;
-            vm->memory[address + 3] = vm->registers[9];
-            break;
-        case CS:
+        case GS:
             vm->memory[address    ] = vm->registers[9] >> 8;
             vm->memory[address + 1] = vm->registers[9];
             break;
-        case EIP:
-            vm->memory[address    ] = vm->registers[10] >> 24;
-            vm->memory[address + 1] = vm->registers[10] >> 16;
-            vm->memory[address + 2] = vm->registers[10] >> 8;
-            vm->memory[address + 3] = vm->registers[10];
-            break;
-        case IP:
-            vm->memory[address    ] = vm->registers[10] >> 8;
+        case SS:
+            vm->memory[address    ] = vm->registers[10]>> 8;
             vm->memory[address + 1] = vm->registers[10];
             break;
+        case CS:
+            vm->memory[address    ] = vm->registers[8] >> 24;
+            vm->memory[address + 1] = vm->registers[8] >> 16;
+            break;
+        case DS:
+            vm->memory[address    ] = vm->registers[9] >> 24;
+            vm->memory[address + 1] = vm->registers[9] >> 16;
+            break;
+        case ES:
+            vm->memory[address    ] = vm->registers[10]>> 24;
+            vm->memory[address + 1] = vm->registers[10]>> 16;
+            break;
+    }
+}
+
+uint8_t
+vm_flags_get(virtual_machine* vm, flags){
+    switch(flags){
+        case flag_carry:     return vm->flags & 0x0001;
+        case flag_parity:    return vm->flags & 0x0002;
+        case flag_auxiliary: return vm->flags & 0x0004;
+        case flag_zero:      return vm->flags & 0x0008;
+        case flag_sign:      return vm->flags & 0x0010;
+        case flag_trap:      return vm->flags & 0x0020;
+        case flag_interrupt: return vm->flags & 0x0040;
+        case flag_direction: return vm->flags & 0x0080;
+        case flag_overflow:  return vm->flags & 0x0100;
+    }
+    return 0;
+}
+
+void
+vm_flags_set(virtual_machine* vm, flags, uint8_t value){
+    switch(flags){
     }
 }
--- virtual_machine.h
+++ virtual_machine.h
@@ -1,68 +1,77 @@
 #ifndef VIRTUAL_MACHINE_H
 #define VIRTUAL_MACHINE_H
 
-// many operating systems automatically zero allocated memory
-// so there isn't really a point into this option
-#define MEMORY_CLEAR_AT_INIT
+/* many operating systems automatically zero allocated memory */
+//#define MEMORY_CLEAR_AT_INIT
+#define MEMORY_RANDOMIZE_AT_INIT
 
 typedef struct{
     uint8_t* memory;
     uint32_t memory_size;
 
-    int32_t registers[13];
-    int16_t flags;
+    int32_t registers[11];
+    int16_t  flags;
+
+    uint32_t MAR;
+    uint32_t PC;
+    uint8_t  IP;
 } virtual_machine;
 
 typedef enum{
-    // general purpose registers
     EAX, AX, AH, AL,
     EBX, BX, BH, BL,
     ECX, CX, CH, CL,
     EDX, DX, DH, DL,
-    // stack registers
-    EDS, DS,
-    EES, ES,
-    EFS, FS,
-    EGS, GS,
-    ESS, SS,
-    ECS, CS,
-    EIP, IP,
-    // instructions
-    MAR,
-    PC
+
+    ESI, SI,
+    EDI, DI,
+    EBP, BP,
+    ESP, SP,
+
+    CS, FS,
+    DS, GS,
+    ES, SS
 } registry;
 
-/**************************************/
+typedef enum{
+    flag_carry,
+    flag_parity,
+    flag_auxiliary,
+    flag_zero,
+    flag_sign,
+    flag_trap,
+    flag_interrupt,
+    flag_direction,
+    flag_overflow
+} flags;
 
-#define VM_REGISTER_GET_U8(vm, reg)\
-    (uint8_t)vm_register_get(vm, reg)
-
-#define VM_REGISTER_GET_16(vm, reg)\
-    (int16_t)vm_register_get(vm, reg)
-
-#define VM_REGISTER_GET_32(vm, reg)\
-    (int32_t)vm_register_get(vm, reg)
-
-/**************************************/
+/* memory */
+#define MEMORY_GET_BYTE(addr)   vm_memory_get_byte(vm, addr)
+#define MEMORY_GET_WORD(addr)   vm_memory_get_word(vm, addr)
+#define MEMORY_GET_DWORD(addr)  vm_memory_get_dword(vm, addr)
 
 /* registers */
-#define VM_REGISTER_GET_BYTE(vm, reg)\
-    (uint8_t)vm_register_get(vm, reg)
+#define REGISTER_SET(reg, val)  vm_register_set(vm, reg, val)
+#define REGISTER_GET_BYTE(reg)  (int8_t)vm_register_get(vm, reg)
+#define REGISTER_GET_WORD(reg)  (int16_t)vm_register_get(vm, reg)
+#define REGISTER_GET_DWORD(reg) (int32_t)vm_register_get(vm, reg)
 
-#define VM_REGISTER_GET_WORD(vm, reg)\
-    (int16_t)vm_register_get(vm, reg)
-
-#define VM_REGISTER_GET_DWORD(vm, reg)\
-    (int32_t)vm_register_get(vm, reg)
+/* flags */
+#define FLAGS_GET(flag)         vm_flags_get(vm, flag)
 
 virtual_machine* virtual_machine_new(uint32_t);
 void    virtual_machine_destroy(virtual_machine*);
+
 int32_t vm_register_get(virtual_machine*, registry);
 void    vm_register_set(virtual_machine*, registry, int32_t);
+
 int8_t  vm_memory_get_byte(virtual_machine*, uint32_t);
 int16_t vm_memory_get_word(virtual_machine*, uint32_t);
 int32_t vm_memory_get_dword(virtual_machine*, uint32_t);
 void    vm_memory_set_register(virtual_machine*, registry, uint32_t);
 void    vm_memory_print(virtual_machine*, uint32_t, uint32_t);
+
+uint8_t vm_flags_get(virtual_machine*, flags);
+void    vm_flags_set(virtual_machine*, flags, uint8_t);
 
 #endif /* VIRTUAL_MACHINE_H */
